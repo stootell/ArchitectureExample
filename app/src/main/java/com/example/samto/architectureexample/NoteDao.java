6979215eba871a811396f660409b1117ed22779b
stootell <samtootell@gmail.com>
2018-11-28 22:03:19 -0600

    Initial commit. First three parts of video tutorial

---
+++ app/src/main/java/com/example/samto/architectureexample/NoteDao.java
@@ -0,0 +1,28 @@
+package com.example.samto.architectureexample;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface NoteDao {
+
+    @Insert
+    void insert(Note note);
+
+    @Update
+    void update(Note note);
+
+    @Delete
+    void delete(Note note);
+
+    @Query("DELETE FROM note_table")
+    void deleteAllNotes();
+
+    @Query("SELECT * FROM NOTE_TABLE ORDER BY priority DESC")
+    List<Note> getAllNotes();
+}
---
+++ app/src/main/java/com/example/samto/architectureexample/NoteDatabase.java
@@ -0,0 +1,22 @@
+package com.example.samto.architectureexample;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+@Database(entities = {Note.class}, version = 1)
+public abstract class NoteDatabase extends RoomDatabase {
+
+    private static NoteDatabase instance;
+
+    public abstract NoteDao noteDao(); // Used to access DAO
+
+    public static synchronized NoteDatabase getInstance (Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context.getApplicationContext(),NoteDatabase.class,
+                    "note_database").fallbackToDestructiveMigration().build();
+        }
+        return instance;
+    }
+}
